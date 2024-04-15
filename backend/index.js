0dcec00870d208b48120cac6eb60b2d0a034306c
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-04-15 17:09:22 +0530

    routing our api

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const connectToMongdb = require('./db');
+
+connectToMongdb();
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
