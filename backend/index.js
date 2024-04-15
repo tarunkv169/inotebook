3311d8d95c2fcf638a16bb6f5805cbab4a6fd543
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-04-15 23:25:32 +0530

    inserting data in mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const connectToMongdb = require('./db');
-
 connectToMongdb();
 
-const express = require('express')
+const express = require('express');
 
 const app = express()
 const port = 3000
+
+// app.use((req, res, next) => {
+//   res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self' http://localhost:3000");
+//   next();
+// });
+
+app.use(express.json());
 
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
