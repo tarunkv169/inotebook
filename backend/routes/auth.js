25b18ac515bfe4dbb3fcd0f35b2dd4c60a670a89
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-04-21 18:05:36 +0530

    storing data in database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,31 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
 const router = express.Router();
+const User = require('../models/User');
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-});
+const { body, validationResult } = require('express-validator');
 
-module.exports = router;
+
+router.post('/',[
+  body('name','enter a valid name').isLength({min: 3}),
+  body('email','enter a valid email address').isEmail(),
+  body('password','password must be atleast 5 character').isLength({min:5}),
+  ],(req,res)=>
+  {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) 
+    {
+      return res.status(400).json({ errors: errors.array() });
+    }
+ 
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err=>{console.log(err)
+     res.json({error: 'please enter a unique value for email',message: err.message})});
+  
+  })
+
+module.exports=router;
