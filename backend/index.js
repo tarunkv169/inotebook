b2be6f681999561207f828adb1945d98be003f5c
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-04-21 18:05:06 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,19 @@
-const connectToMongdb = require('./db');
-connectToMongdb();
+const mongodb_connect = require('./db');
 
-const express = require('express');
+mongodb_connect();
 
+const express = require('express')
 const app = express()
 const port = 3000
 
-// app.use((req, res, next) => {
-//   res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self' http://localhost:3000");
-//   next();
-// });
-
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 app.use(express.json());
-
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+
 })
