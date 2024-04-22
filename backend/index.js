b9ca8b82d038c31416424be537f2d298b8e3b509
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-04-22 15:21:50 +0530

    changing port

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,18 @@
-const mongodb_connect = require('./db');
 
-mongodb_connect();
+const connectToMongdb = require('./db');
+connectToMongdb();
 
-const express = require('express')
+const express = require('express');
+
 const app = express()
-const port = 3000
+const port = 5000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+
 app.use(express.json());
+
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-
 })
