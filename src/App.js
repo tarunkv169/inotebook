9417828ed1a828c73fc9c92a368931aebdf93f9e
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-04-26 12:10:50 +0530

    NoteState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/Home" element={<Home />}></Route>
+            <Route path="/About" element={<About />}></Route>
+          </Routes>
+        </Router>
+      </NoteState>
     </div>
   );
 }
