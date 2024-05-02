31272281df538ab3a8abe36a9cd3fe01970d01dd
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-05-02 15:44:43 +0530

    uselocation in /Home and /About

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Navbar() {
+  let location = useLocation();
+  useEffect(()=>{
+    console.log(location.pathname)
+  },[location])
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
@@ -12,10 +16,10 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/Home">Home</Link>
+          <Link className= {`nav-link ${location.pathname==="/Home"?"active":""}`} aria-current="page" to="/Home">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/About">About</Link>
+          <Link className={`nav-link ${location.pathname==="/About"? "active":" "}`} to="/About">About</Link>
         </li>
         <li className="nav-item dropdown">
           <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
