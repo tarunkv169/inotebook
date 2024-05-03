29a771deece010e6effe85fee2090bfc8f1924c2
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-05-03 23:50:52 +0530

    comments which show working of context

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,14 +1,18 @@
+// now using noteContext we made components to access "state" defined in this "NoteState.js"
 import noteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState=(props)=>{
+    //  s1 obj
     let s1={
         name:"tarun",
         class:"5b"
     }
 
+    // giving state a default value s1
     const[state,setState] = useState(s1);
 
+    // using update func to change state value
     let update=()=>{
         setTimeout(()=>{
            setState({
@@ -18,6 +22,7 @@ const NoteState=(props)=>{
         },2000)
     }
 
+    // then returning state and update to be use by other components
     return(
         <noteContext.Provider value={{state,update}}>
             {props.children};
@@ -26,4 +31,5 @@ const NoteState=(props)=>{
 }
 
 
+// exporting to be use by App.js see wrap all return in <NoteState>
 export default NoteState;
--- src/context/notes/noteContext.js
+++ src/context/notes/noteContext.js
@@ -1,5 +1,8 @@
+// react i want to use Context Api so let me create a context
 import { createContext } from "react";
 
+// ok creating notecontext
 const noteContext = createContext();
 
+// exporting so to be use by "NoteState" and by the "components"
 export default noteContext;
