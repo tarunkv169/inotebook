672970e266e6380411bce4c38546fb97d4d807fc
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-05-03 23:52:25 +0530

    example of using context in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,17 @@ import noteContext from '../context/notes/noteContext'
 
 export default function About() {
 
-  const a = useContext(noteContext);
+  const a = useContext(noteContext);   // with help of useContext(hook) we use "noteContext powers" given to a
   useEffect(()=>{
     a.update();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
+
+  // as name and class is in state so using state.name and state.class
   return (
     <div>
-       this is {a.state.name} and read in {a.state.class};
+       this is {a.state.name} and read in {a.state.class};  
     </div>
   )
 }
