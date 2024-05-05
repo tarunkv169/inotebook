ed9bcbbdd16d2e6e360804131dad6fc35c481e15
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-05-06 01:45:59 +0530

    calling updatenote() in Note.js by clicking edit icon

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -2,15 +2,27 @@ import React, { useContext } from 'react'
 import noteContext from '../context/notes/noteContext';
 
 export default function Noteitem(props) {
-    const {note}=props;                         //----->come from 4️⃣---->4️⃣.1️⃣  using (note of notes) we make card of note
+    const {note,updatenote}=props;                         //7️⃣.1️⃣using (note of notes) we make card of note and 🛑getting id of this particular note  <------7️⃣.1️⃣---from Note.js------
+                                                     //8️⃣.2️⃣ using updatenote from props and put in edit icon
 
-    const {deletenote} = useContext(noteContext);
-    const handledelete=(e)=>{
+
+
+
+    const {deletenote} = useContext(noteContext);  //7️⃣.2️⃣ getting func form noteContext
+    const handledelete=(e)=>{                    // 7️⃣.5️⃣ on click on delete icon ---passing (id of note) to deletenote() in NoteState.js-------->7️⃣.6️⃣------>NoteState.js
        e.preventDefault();
         deletenote(note._id);
     }
-  return (                                      // 4️⃣.2️⃣ (col-md-3) is use to show row wise data
-                                                // 4️⃣.3️⃣ (d-flex) and inside h5(p-2 flex-grow-1) is used to customize icon (i-took from font awesome)
+
+
+
+
+
+    
+
+  return (                                      // 7️⃣.3️⃣ (col-md-3) is use to show row wise data
+                                                // 7️⃣.4️⃣ (d-flex) and inside h5(p-2 flex-grow-1) is used to customize icon (i-took from font awesome)
+                                                // 8️⃣.3️⃣ onClick edit icon call ( 🛑updatenote() with parameter of currentnote data(title,des,tag) of editing note ) ------------->8️⃣.4️⃣Note.js--->
     <div className="col-md-3">                  
       <div className="card">
         <div className="card-body">
@@ -18,7 +30,7 @@ export default function Noteitem(props) {
           <div className="d-flex">              
              <h5 className="card-title p-2 flex-grow-1">{note.title}</h5>
              <i className="fa-solid fa-trash-can mx-2" onClick={handledelete}></i>
-             <i className="fa-solid fa-pen-to-square mx-2"></i>
+             <i className="fa-solid fa-pen-to-square mx-2" onClick={()=>{updatenote(note)}}></i>    
           </div>  
 
              <p className="card-text">{note.description}</p> 
