be6ece85c8fa68438c25376bcd19325fbfd254af
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-05-06 01:45:14 +0530

    updatenote() to retrive already note and handlechange() to update note and updateclick() to call editnote()

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useContext } from 'react'
 import noteContext from '../context/notes/noteContext'
 import Noteitem from './Noteitem';
@@ -7,29 +7,104 @@ import Addnote from './Addnote';
 export default function Note() {
   
     const ncontext=useContext(noteContext);                         //1️⃣starting from here  use the noteContext
-    const {notes,getnote} = ncontext;                               //2️⃣ in {} which we want to use we can use------>go to NoteState2️⃣.1️⃣------>
+    const {notes,getnote,editnote} = ncontext;                               //2️⃣ in {} which we want to use we can use------>go to NoteState2️⃣.1️⃣------>
 
 
 
 
-    useEffect(()=>{                                                 //2️⃣.6️⃣useEffect() is applied when something is changing so in getnote() we do setNotes  2️⃣.6️⃣ <----------------
-        getnote();                                                  //2️⃣.7️⃣open backend terminal--->"npm install cors" and import( var cors = require('cors') and app.use(cors())) in index.js of backend
+    useEffect(()=>{                                                 //2️⃣.5️⃣useEffect() is applied when something is changing so in getnote() we do setNotes  2️⃣.5️⃣ <----------------from NoteState.js
+        getnote();                                                  //2️⃣.6️⃣open backend terminal--->"npm install cors" and import( var cors = require('cors') and app.use(cors())) in index.js of backend
         // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
   
+    
+    
 
 
-    return (                                                        //5️⃣ Addnote comp
+
+
+
+
+    const [note,setNote] = useState({etitle:"",edescription:"",etag:""});  //8️⃣.4️⃣.1️⃣ initialize with empty 
+
+    const ref = useRef(null);
+    const refClose = useRef(null);
+    const updatenote=(currentnote)=>{   //8️⃣.4️⃣--- ⬆️-(8️⃣.4️⃣.1️⃣)---when edit icon click this func will run and setNote(with currentdata of editing note)
+        
+        setNote({etitle: currentnote.title, edescription: currentnote.description, etag: currentnote.tag, id: currentnote._id})
+        ref.current.click();            //8️⃣.5️⃣---useref of current define (in className="btn btn-primary d-none" do ref={ref}) and (outer do const ref= useRef(null))
+    }
+
+
+    const handlechange=(e)=>{          //8️⃣.6️⃣---onChange--setNote(with oldnote+newnote)
+        setNote({...note,[e.target.name]: e.target.value})
+    }
+
+
+    const updateclick=(currentnote)=>{   //8️⃣.7️⃣---onClick--call--editnote()--with parameter of edited note to save in database and show on UI
+        editnote(note.etitle , note.edescription , note.etag , note.id)
+        refClose.current.click();        //8️⃣.8️⃣---(in close button ref={refClose}) and (outer do const refClose= useRef(null))------------------->8️⃣.9️⃣go to NoteState.js---->
+    }
+
+
+
+
+
+
+
+
+
+    return (                                                        //5️⃣.0️⃣ Addnote comp ---------->goto Addnote.js5️⃣.1️⃣------>
+                                                                    //8️⃣.0️⃣ creating modal   and   inserting(addnote form in className="modal-body") change id,name,className
         <>
-        <Addnote/>                                                 
-        <div className="row">
+        <Addnote/>     
+        
+        <button type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop" ref={ref}>
+        </button>
 
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">Edit Note</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+               <form>
+                   <div className="mb-3 my-4">
+                       <label htmlFor="etitle" className="form-label">title</label>
+                       <input type="text" className="form-control" id="etitle" name='etitle'  onChange={handlechange} />
+                   </div>
+                   <div className="mb-3">
+                       <label htmlFor="edescription" className="form-label">description</label>
+                       <input type="text" className="form-control" id="edescription" name='edescription' onChange={handlechange} />
+                   </div>
+                   <div className="mb-3">
+                       <label htmlFor="etag" className="form-label">tag</label>
+                       <input type="text" className="form-control" id="etag" name='etag' onChange={handlechange} />
+                   </div>
+  
+               </form>
+            </div>
+            <div className="modal-footer">
+              <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary" onClick={updateclick}>Update Notes</button>
+            </div>
+          </div>
+        </div>
+        </div>  
+
+
+
+
+
+        <div className="row">
             <h2>Your Notes</h2>
 
             {  
-                notes.map((note)=>{                                 //3️⃣ using map() we use Noteitem.js because in this we integrate the fetch_notes
-                    return <Noteitem key={note._id} note={note}/>   //4️⃣ now we make Noteitem.js in which we send (each note of notes) -----====> go to 4️⃣.1️⃣---------->
-                })
+                notes.map((note)=>{                                 //6️⃣ using map() we use Noteitem.js because in this we integrate the fetch_notes
+                    return <Noteitem key={note._id} updatenote={updatenote} note={note}/>   //7️⃣.0️⃣ now we make Noteitem.js in which we send (each note of notes) -----====> go to Noteitem.js with(🛑note={note}7️⃣.1️⃣---------->
+                })                                                                          //8️⃣.1️⃣---(🛑updatenote(updatenote))passing to Noteitme--------------8️⃣.2️⃣go to Noteitem.js------> -->
             }
 
         </div>
