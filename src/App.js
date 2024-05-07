b4ebf621a24099e90c8bbfa3c79aa628708ca588
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-05-07 18:31:51 +0530

    do react-router-dom of login and signup components

--- src/App.js
+++ src/App.js
@@ -5,21 +5,30 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
 
 function App() {
   return (
+    
     <div className="App">
       <NoteState>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/Home" element={<Home />}></Route>
-            <Route path="/About" element={<About />}></Route>
-          </Routes>
-        </Router>
+        <div className="container">
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/About" element={<About />}></Route>
+              <Route path="/Login" element={<Login />}></Route>             
+              <Route path="/Signup" element={<Signup />}></Route>
+            </Routes>
+          </Router>
+        </div>
       </NoteState>
     </div>
-  );
-}
+    
+  );                                                             // 9️⃣.1️⃣do(put login and signup comp in Route)<--------from Navbar.js
+}                                                                // go to login comp------->9️⃣.2️⃣-login.js---->
 
 export default App;
